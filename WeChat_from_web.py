e4c45a4d85458d9c0fc5d5a6f2886d62b424ce86
root <root@22-host.114.114.114.114>
2020-09-22 01:19:47 +0800

    v1

---
+++ WeChat_from_web.py
@@ -0,0 +1,113 @@
+﻿from selenium import webdriver
+from xml.dom.minidom import parse
+import time,os,requests
+import threading
+
+
+#----------------------------提交操作------------------------------------
+class action_Submit(threading.Thread):
+    def __init__(self,files_path,Images_path,chrome_path):
+        super(action_Submit, self).__init__()
+        self.files_path=files_path
+        self.Images_path=Images_path
+        self.chrome_path=chrome_path
+
+        self.daemon = True  # Allow main to exit even if still running.
+        self.paused = True  # Start out paused.
+        self.state = threading.Condition()
+
+# URL检测站点正常！
+    def getHttpStatusCode(self,url):
+        try:
+            request = requests.get(url)
+            httpStatusCode = request.status_code
+            return url,httpStatusCode
+        except requests.exceptions.HTTPError as e:
+            return e
+
+#保证微信消息保存成xml
+    def Start_saver_xml(self,msg,file_path):
+        msg_files = open(file_path, 'w', encoding='utf-8')
+        try:
+            msg_files.write(msg)
+        except Exception as e:
+            msg_files.close()
+            print(e)
+
+#提交时并保存图片到目录下
+    def saver_images(self):
+        file_name  = time.strftime("%Y-%m-%d_%H-%M-%S", time.localtime(time.time()))
+        dest_dir = self.Images_path+file_name+'.jpg'
+        return dest_dir
+
+#main操作方法--------------------------
+    def run_main(self,url):
+        driver = webdriver.Chrome(self.chrome_path)
+        #driver.set_window_size(840, 600)
+        driver.maximize_window()
+        driver.get(str(url))
+        time.sleep(50)
+
+        try:
+            ####输入姓名----------ok----------------
+            input_text = driver.find_element_by_id("q1")
+            input_text.send_keys("陈艺彬")
+            driver.get_screenshot_as_file(self.saver_images())
+            ####选择系统运行----------ok----------------
+            answers = driver.find_elements_by_css_selector('.ui-controlgroup')
+            driver.find_elements_by_class_name("ui-checkbox")[50].click()
+            driver.find_elements_by_class_name("ui-checkbox")[78].click()
+            driver.get_screenshot_as_file(self.saver_images())
+            ####选择系统运行状态----------ok----------------
+            driver.find_elements_by_class_name("ui-radio")[0].click()
+            time.sleep(6)
+            btn_reg = driver.find_element_by_id('ctlNext')
+            btn_reg.click()
+            time.sleep(6)
+            driver.get_screenshot_as_file(self.saver_images())
+
+        except Exception as e:
+            print("代码运行出错： ERROR", format(e))
+            driver.quit()
+        ###关闭浏览器窗口----------ok----------------
+        time.sleep(8)
+        driver.quit()
+
+#线程控制器------------------------------------------------------------
+    def resume(self):  # 用来恢复/启动run
+        with self.state:  # 在该条件下操作
+            self.paused = False
+            self.state.notify()  # Unblock self if waiting.
+
+    def pause(self):  # 用来暂停run
+        with self.state:  # 在该条件下操作
+            self.paused = True  # Block self.
+
+#线程操作主方法
+    def run(self):
+        while True:
+            with self.state:
+                if os.path.isfile(self.files_path):
+                    count=0
+                    while count < 4:
+                        DOM_Tree = parse(self.files_path)
+                        url = DOM_Tree.getElementsByTagName('url')[0].childNodes[0].data
+                        HTTPS_URL,STATUS=self.getHttpStatusCode(url)
+                        if STATUS == 200:
+                            self.run_main(HTTPS_URL)
+                            count+=1
+                            time.sleep(30)
+                    os.remove(self.files_path)
+
+if __name__ == "__main__":
+    #files_path = 'F:\\WeChat_app\\cache_msg.xml'
+    #Images_path = 'F:\\WeChat_app\\images\\'
+    #chrome_path='F:\\WeChat_app\\chromedriver.exe'
+
+    files_path = 'C:\\Program Files\\Microsoft Games\\WeChat_app\\cache_msg.xml'
+    Images_path = 'C:\\Program Files\\Microsoft Games\\WeChat_app\\images\\'
+    chrome_path = 'C:\\Program Files\\Microsoft Games\\WeChat_app\\chromedriver.exe'
+    app=action_Submit(files_path,Images_path,chrome_path)
+    app.start()
+
+
---
+++ run_app.py
@@ -0,0 +1,87 @@
+﻿from WechatPCAPI import WechatPCAPI
+from WeChat_from_web import action_Submit
+import time
+import logging
+from queue import Queue
+import threading,os
+
+# 监听群名
+group_receive_list = ['程序测试']
+#group_receive_list = ['03 HW-2020值守及应急处置工作群']
+
+#初始化程序
+logging.basicConfig(level=logging.INFO)
+queue_recved_message = Queue()
+
+#将消息送进队列
+def on_message(message):
+    queue_recved_message.put(message)
+
+
+#线程控制台
+def thread_handle_message(wx_inst,app,xml_path):
+    # 线程指针
+    One_Status=True
+
+    while True:
+        #取到所有消息队列
+        message = queue_recved_message.get()
+        #print("微信实时接收到的信息：-----------------------------" +str(message))
+
+        # 消息对列取出群名
+        from_chatroom_nickname = message.get(
+            'data', {}).get('from_chatroom_nickname', '')
+
+        # 消息对列取出消息
+        from_chatroom_nickname_msg=message.get(
+            'data', {}).get('msg', '')
+
+        #判断是否是我们想监听的群  不是不处理
+        if from_chatroom_nickname in group_receive_list:
+            #判断消息xml文件是否包含url 并可访问
+            try:
+                if not from_chatroom_nickname_msg.find('<?xml version="1.0"?>'):
+                    print(">>>>>>>>>接收到过滤URL--->>>>>>>正在保存！--->>>>>>>准备填表>>>>>>>>>>>>")
+                    #将消息保存成xml
+                    if not os.path.isfile(xml_path):
+                            app.Start_saver_xml(from_chatroom_nickname_msg,xml_path)
+                            #app.setDaemon(True)
+                            print("开始填表！")
+                            if One_Status:
+                                app.start()
+                                app.join()
+                                app.pause()
+                                One_Status=False
+                            print("文件是否存-1：",os.path.isfile(xml_path))
+                            if not os.path.isfile(xml_path):
+                                app.pause()
+                            if os.path.isfile(xml_path):
+                                app.resume()
+                            print("文件是否-2：", os.path.isfile(xml_path))
+
+
+                print("监听工作群接收的信息：---->>>" +str(from_chatroom_nickname_msg))
+                print("继续等待接收消息 -----------------------!")
+            except Exception as e:
+                print("代码运行出错： ERROR", format(e))
+
+#主方法
+def main():
+    xml_path='C:\\Program Files\\Microsoft Games\\WeChat_app\\cache_msg.xml'
+    Images_path = 'C:\\Program Files\\Microsoft Games\\WeChat_app\\images\\'
+    chrome_path = 'C:\\Program Files\\Microsoft Games\\WeChat_app\\chromedriver.exe'
+
+    wx_inst = WechatPCAPI(on_message=on_message, log=logging)
+    wx_inst.start_wechat(block=True)
+    while not wx_inst.get_myself():
+        time.sleep(5)
+        print('登陆成功')
+        #填表应用
+        app = action_Submit(files_path=xml_path, Images_path=Images_path, chrome_path=chrome_path)
+        #微信监听app
+        threading.Thread(target=thread_handle_message, args=(wx_inst,app,xml_path)).start()
+        time.sleep(5)
+
+#开始运行
+if __name__ == '__main__':
+    main()
